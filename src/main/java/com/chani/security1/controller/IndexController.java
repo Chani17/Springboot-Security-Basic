86474d36d23173596385ab4b36e93053c4f9c88b
Chani17 <cucute8878@naver.com>
2021-09-08 01:47:16 +0900

    basic security completed!

--- src/main/java/com/chani/security1/controller/IndexController.java
+++ src/main/java/com/chani/security1/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.chani.security1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -63,5 +65,17 @@ public class IndexController {
 		
 		userRepository.save(user);	// 회원가입 잘됨. 비밀번호 1234 => 시큐리티 로그인을 할 수 없음. 이유는 패스워드가 암호화가 안되었기 때문!
 		return "redirect:/loginForm";
+	}
+	
+	@Secured("ROLE_ADMIN")
+	@GetMapping("/info")
+	public @ResponseBody String info() {
+		return "개인정보";
+	}
+	
+	@PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")		// data()가 실행되기 직전에 PreAuthorize가 실행됨
+	@GetMapping("/data")
+	public @ResponseBody String data() {
+		return "데이터정보";
 	}
 }
